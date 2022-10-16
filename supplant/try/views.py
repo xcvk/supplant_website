f93b29adce93eaddbdfe3d01e3a3be71fbb58d0d
021478963 <alanhe159@gmail.com>
2022-10-16 01:07:41 -0400

    fix import

--- supplant/try/views.py
+++ supplant/try/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from .forms import GetInput
 from django.http import HttpResponse
-from .algorithm.algorithm_placeholder import algor
+from .supplant_logic.algorithm_placeholder import algor
 
-# Create your views here.
 def index(request):
+  # if method is post, then user has sent some data. Change the resulting page.
   if request.method == "POST":
     answer = GetInput(request.POST)
 
     if answer.is_valid():
       data = answer.cleaned_data["to_fix"]
       form = GetInput({"to_fix":data})
+      # fix the data here
       result = algor(data)
     return render(request, 'try_filled.html', {"form":form, "result":result})
 
   else:
-    form = GetInput({"to_fix":"hello"})
+    form = GetInput({"to_fix":"type here"})
     return render(request, 'try.html', {"form":form})
